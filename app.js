91ea4bea1f52d6063371b47a064eec554f551a98
Purvesh Rathod <purvesh62@github.com>
2020-05-04 13:35:45 +0530

    Select audio changes

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ class Drumkit {
     constructor() {
         this.pads = document.querySelectorAll(".pad");
         this.playBtn = document.querySelector(".play");
+        this.currentKick = "./allSounds/kick-classic.wav"
+        this.currentSnare = "./allSounds/snare-acoustic01.wav"
+        this.currentHihat = "./allSounds/kick-acoustic01.wav"
         this.kickAudio = document.querySelector(".kick-sound");
         this.snareAudio = document.querySelector(".snare-sound");
         this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
         this.bpm = 150;
         this.isPlaying = null;
+        this.selects = document.querySelectorAll("select");
+        this.muteBtn = document.querySelectorAll(".mute");
     }
 
     activePad() {
@@ -70,9 +75,33 @@ class Drumkit {
             this.playBtn.classList.remove("active")
         }
     }
+    changeSound(e) {
+        console.log(e);
+        // Gives name of select
+        const selectionName = e.target.name;
+        const selectionValue = e.target.value;
+        switch (selectionName) {
+            case "kick-select":
+                this.kickAudio.src = selectionValue;
+                break;
+            case "snare-select":
+                this.snareAudio.src = selectionValue;
+                break;
+            case "hihat-select":
+                this.hihatAudio.src = selectionValue;
+                break;
+        }
+    }
+    // mute(e) {
+    //     const muteIndex = e.target.getAttribute("data-track");
+    //     e.target.classList.toggle("active");
+    //     console.log(muteIndex);
+    // }
 }
 
 const drumkit = new Drumkit();
+
+// Event Listners
 
 drumkit.pads.forEach(pad => {
     pad.addEventListener("click", drumkit.activePad);
@@ -87,3 +116,15 @@ drumkit.playBtn.addEventListener("click", function () {
     drumkit.updateBtn();
     drumkit.start();
 });
+
+drumkit.selects.forEach(select => {
+    select.addEventListener("change", function (e) {
+        drumkit.changeSound(e);
+    })
+});
+
+// drumkit.muteBtn.forEach(btn => {
+//     btn.addEventListener('click', function (e) {
+//         drumkit.mute(e);
+//     })
+// })
